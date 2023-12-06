6c5c6957494511b99230ab0831b2fea3b1829d87
Michal Moskal <michal@moskal.me>
2023-12-06 00:13:51 +0000

    fix optimization flags

--- setup.py
+++ setup.py
@@ -14,8 +14,8 @@ ROOT_DIR = os.path.dirname(__file__)
 # Compiler flags.
 CXX_FLAGS = ["-g", "-O2", "-std=c++17"]
 # TODO(woosuk): Should we use -O3?
-#NVCC_FLAGS = ["-O2", "-std=c++17"]
-NVCC_FLAGS = ["-g", "-G", "-O0", "-std=c++17"]
+NVCC_FLAGS = ["-O2", "-std=c++17"]
+#NVCC_FLAGS = ["-g", "-G", "-O0", "-std=c++17"]
 
 ABI = 1 if torch._C._GLIBCXX_USE_CXX11_ABI else 0
 CXX_FLAGS += [f"-D_GLIBCXX_USE_CXX11_ABI={ABI}"]
